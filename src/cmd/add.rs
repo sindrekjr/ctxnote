74094f0a6a3cf912ef76c246f119a303067035ad
Sindre S. Kjær <sindre@sindrekjr.no>
2022-01-19 18:26:10 +0100

    refactor: rename put -> add

--- src/cmd/add.rs
+++ src/cmd/add.rs
@@ -2,11 +2,11 @@ use crate::CmdHandling;
 use clap::Parser;
 
 #[derive(Parser)]
-pub struct PutCmd {
+pub struct AddCmd {
     entry: String,
 }
 
-impl CmdHandling for PutCmd {
+impl CmdHandling for AddCmd {
     fn handle(&self) -> Result<String, String> {
         Ok(format!("Put ran to completion with entry: {}", self.entry))
     }
--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -1,6 +1,6 @@
+mod add;
 mod conf;
 mod ctx;
-mod put;
 
 use clap::Parser;
 
@@ -10,7 +10,7 @@ pub trait CmdHandling {
 
 #[derive(Parser)]
 enum Cmd {
-    Put(put::PutCmd),
+    Add(add::AddCmd),
     Conf(conf::ConfCmd),
     Ctx(ctx::CtxCmd),
     Init(ctx::CtxInitCmd),
@@ -26,7 +26,7 @@ pub struct NoteCmd {
 impl CmdHandling for NoteCmd {
     fn handle(&self) -> Result<String, String> {
         match &self.cmd {
-            Cmd::Put(put) => put.handle(),
+            Cmd::Add(add) => add.handle(),
             Cmd::Conf(conf) => conf.handle(),
             Cmd::Ctx(ctx) => ctx.handle(),
             Cmd::Init(init) => init.handle(),
